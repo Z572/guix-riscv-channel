0db1e7b939c29294e5bfd40a9108e270326dd0a2
Zheng Junjie <z572@z572.online>
2025-04-01 02:21:27 +0800

    images: megrez: use parallel-build and without tests's guix.

--- riscv/system/images/megrez.scm
+++ riscv/system/images/megrez.scm
@@ -2,6 +2,7 @@
 (define-module (riscv system images megrez)
   #:use-module (gnu system linux-initrd)
   #:use-module (gnu packages base)
+  #:use-module (guix utils)
   #:use-module (guix inferior)
   #:use-module (guix channels)
   #:use-module (srfi srfi-1)
@@ -18,7 +19,7 @@
   #:use-module (gnu packages linux)
   #:use-module (guix gexp)
   #:use-module (guix packages)
-
+  #:use-module (gnu packages package-management)
   #:use-module (gnu packages ssh)
   #:use-module (gnu packages guile-xyz)
   #:use-module (gnu packages admin)
@@ -35,6 +36,19 @@
             megrez-image-type
             megrez-barebones-raw-image))
 
+
+(define guix*
+  (package/inherit guix
+    (arguments
+     (substitute-keyword-arguments (package-arguments guix)
+       ((#:parallel-build? parallel-build? #t)
+        (if (%current-target-system)
+            parallel-build?
+            #t))
+       ((#:tests? test? #f)
+        (if (%current-target-system)
+            test?
+            #f))))))
 
 (define megrez-barebones-os
   (operating-system
@@ -67,6 +81,7 @@
                (guix-service-type config =>
                                   (guix-configuration
                                    (inherit config)
+                                   (guix guix*)
                                    (substitute-urls
                                     (list "https://ci.z572.online"
                                           "https://bordeaux.guix.gnu.org" ))
@@ -80,7 +95,8 @@
   (q #166927E5E329B2AF7E965A4AE07403B69177EB0B8556D3467A83E0BE5E3D27F9#)
   )
  )"))
-                                     %default-authorized-guix-keys))
+                                     (list (file-append guix* "/share/guix/berlin.guix.gnu.org.pub")
+                                           (file-append guix* "/share/guix/bordeaux.guix.gnu.org.pub"))))
                                    (discover? #t)
                                    (extra-options
                                     (list "-M 4" "-c 4")))))))))
